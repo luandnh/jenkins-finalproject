8158f86a479f36c23d27b34d6e88f3aea98082fe
luandnh <luandnh1998@gmail.com>
2020-11-01 21:47:17 +0700

    fix

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sh 'docker build -t ${DOCKER_IMAGE}:${BUILD_ID} .'
 sh 'docker tag ${DOCKER_IMAGE}:${BUILD_ID} ${DOCKER_IMAGE}:lastest'
 sh 'docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PW'
